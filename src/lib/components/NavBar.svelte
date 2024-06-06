57ffd5ff0a1f48f965a6fb3c88f4f4cfa3d2791e
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-06 13:25:33 -0400

    navbar handle conditionals

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,16 +1,30 @@
+<script>
+	import Button from './ui/button/button.svelte';
+
+	// TODO: pass only a user in props
+	export let signedIn = false;
+	export let username = '';
+</script>
+
 <div class="border-b">
 	<div class="flex h-16 items-center px-4">
 		<a href="/" class="font-bold text-2xl">Dark Pomodoro</a>
 		<div class="flex items-center space-x-4 lg:space-x-6 mx-6">
-			<a href="/" class="text-sm font-medium transition-colors hover:text-primary"> Pomodoro </a>
+			<a href="/" class="text-sm font-medium transition-colors hover:text-primary">Pomodoro</a>
 
 			<a
 				href="/"
 				class="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
 			>
-				Pomodoro
+				Statistics
 			</a>
 		</div>
-		<div class="ml-auto flex items-center space-x-4">Welcome, CoolMan123</div>
+		<div class="ml-auto flex items-center space-x-4 text-sm">
+			{#if signedIn}
+				Welcome, {username}
+			{:else}
+				<Button>Sign In</Button>
+			{/if}
+		</div>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Dark Pomodoro</title>
+</svelte:head>
+
 <div class="flex flex-col">
 	<NavBar />
 	<div class="flex-1 space-y-4 p-8 pt-6">
