e2cd6919334600f297de7a1496a9f2f2de270dee
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-06 16:54:26 -0400

    add task list placeholder

---
+++ src/lib/components/TaskList.svelte
@@ -0,0 +1,15 @@
+<script>
+	import * as Card from './ui/card';
+
+	/** @type {string | undefined} */
+	let className = undefined;
+	export { className as class };
+</script>
+
+<Card.Root class={className}>
+	<Card.Header>
+		<Card.Title>Task List</Card.Title>
+		<Card.Description>A minimalistic task list to keep track of your work.</Card.Description>
+	</Card.Header>
+	<Card.Content></Card.Content>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
-	import { page } from '$app/stores';
+	import TaskList from '$lib/components/TaskList.svelte';
 	import Timer from '$lib/components/Timer.svelte';
 </script>
 
 <main>
-	<div class="grid grid-cols-2">
-		<Timer class="max-w-sm" />
+	<div class="flex gap-3">
+		<Timer class="w-full max-w-lg" />
+		<TaskList class="w-full" />
 	</div>
 </main>
