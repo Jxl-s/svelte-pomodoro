23bcbe546850933d842455f4b29e8da004428fe6
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-06 17:17:50 -0400

    add todo and completed sections

--- src/lib/components/TaskList.svelte
+++ src/lib/components/TaskList.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Button from './ui/button/button.svelte';
 	import * as Card from './ui/card';
+	import Input from './ui/input/input.svelte';
 
 	/** @type {string | undefined} */
 	let className = undefined;
@@ -11,5 +13,20 @@
 		<Card.Title>Task List</Card.Title>
 		<Card.Description>A minimalistic task list to keep track of your work.</Card.Description>
 	</Card.Header>
-	<Card.Content></Card.Content>
+	<Card.Content>
+		<div class="flex gap-2 max-w-sm">
+			<Input placeholder="Task name" />
+			<Button size="sm">+ Task</Button>
+		</div>
+		<div class="mt-4 grid lg:grid-cols-2">
+			<div>
+				<p class="font-semibold">To Do</p>
+				<p class="text-muted-foreground text-sm">List of tasks left to do</p>
+			</div>
+			<div>
+				<p class="font-semibold">Completed</p>
+				<p class="text-muted-foreground text-sm">List of completed tasks for the session</p>
+			</div>
+		</div>
+	</Card.Content>
 </Card.Root>
