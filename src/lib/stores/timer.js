fb35200e5cd47f9d03462cbdf9008a8cd883b0e2
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-06 00:40:47 -0400

    refactor timer store

--- src/lib/stores/timer.js
+++ src/lib/stores/timer.js
@@ -1,16 +1,47 @@
 import { writable } from 'svelte/store';
 
-export const TimerState = {
-	None: 'None',
+// Defaults
+const DEFAULT_FOCUS_TIME = 25 * 60 * 1000;
+const DEFAULT_SHORT_BREAK_TIME = 5 * 60 * 1000;
+const DEFAULT_LONG_BREAK_TIME = 15 * 60 * 1000;
+
+const DEFAULT_SOUND_ENABLED = true;
+const DEFAULT_NOTIFICATION_ENABLED = true;
+
+// Enums
+export const SessionTypes = {
 	Focus: 'Focus',
 	ShortBreak: 'ShortBreak',
 	LongBreak: 'LongBreak'
 };
 
-export const timerState = writable(TimerState.None);
-export const timerValue = writable(0);
-export const timerStarted = writable(false);
-export const timerPaused = writable(false);
+export const TimerStatus = {
+	Running: 'Running',
+	Paused: 'Paused',
+	Stopped: 'Stopped'
+};
+
+export const createTimerStore = () => {
+	const settings = writable({
+		// Durations
+		focusTime: DEFAULT_FOCUS_TIME,
+		shortBreak: DEFAULT_SHORT_BREAK_TIME,
+		longBreak: DEFAULT_LONG_BREAK_TIME,
+
+		// Other
+		soundEnabled: DEFAULT_SOUND_ENABLED,
+		notificationEnabled: DEFAULT_NOTIFICATION_ENABLED
+	});
+
+	const session = writable({
+		type: SessionTypes.Focus,
+		timeLeft: DEFAULT_FOCUS_TIME,
+		timerStatus: TimerStatus.Stopped,
+		completedCount: 0
+	});
+
+	return { settings, session };
+};
 
 /**
  * Format the time in seconds to a human readable format
@@ -24,21 +55,4 @@ export function formatTime(seconds) {
 	const paddedMinutes = minutes.toString().padStart(2, '0');
 	const paddedSeconds = remainingSeconds.toString().padStart(2, '0');
 	return `${paddedMinutes}:${paddedSeconds}`;
-}
-
-export function nextState() {
-	timerState.update((state) => {
-		switch (state) {
-			case TimerState.None:
-				return TimerState.Focus;
-			case TimerState.Focus:
-				return TimerState.ShortBreak;
-			case TimerState.ShortBreak:
-				return TimerState.Focus;
-			case TimerState.LongBreak:
-				return TimerState.Focus;
-			default:
-				return TimerState.None;
-		}
-	});
 }
