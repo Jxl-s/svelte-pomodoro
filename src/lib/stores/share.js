e19b26fef47e6ecf161b81f381823f7db72e85a9
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2024-06-07 15:53:24 -0400

    hide shared session

--- src/lib/stores/share.js
+++ src/lib/stores/share.js
@@ -3,6 +3,7 @@ import { writable } from 'svelte/store';
 const createShareStore = () => {
 	const isMultiplayer = writable(false);
 	const isHost = writable(false);
+
 	return { isMultiplayer, isHost };
 };
 
